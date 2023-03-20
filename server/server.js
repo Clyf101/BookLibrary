c294b26f7e653c28ca723a2e42fcaa6a8a6c0a50
Clyford <hclyford@gmail.com>
2023-03-19 19:36:49 -0700

    add files

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
-const { typeDefs, resolvers } = require('./schema');
 const routes = require('./routes');
 
 const app = express();
@@ -17,9 +15,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
-
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
